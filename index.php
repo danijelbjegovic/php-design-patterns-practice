a409c309964e4632a0e4ac1da60a2e72f842a3b7
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-09-19 11:34:08 +0200

    Decoration pattern with car services as example

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+interface CarService{
+	public function getCost();
+	public function getDescription();
+}
+
+
+
+
+
+class BasicInspection implements CarService{
+	public function getCost(){
+		return 25;
+	}
+
+	public function getDescription(){
+		return 'Basic Inspection';
+	}
+}
+
+
+
+
+
+class OilChange implements CarService{
+
+	protected $carService;
+
+	public function __construct(CarService $carService)
+	{
+		$this->carService = $carService;
+	}
+
+	public function getCost(){
+		return 29 + $this->carService->getCost();
+	}
+	public function getDescription(){
+		return $this->carService->getDescription() . ', and oil change';
+	}
+
+}
+
+
+class TireRotation implements CarService{
+	protected $carService;
+
+	public function __construct(CarService $carService){
+		$this->carService = $carService;
+	}
+
+	public function getCost(){
+		return 10 + $this->carService->getCost();
+	}
+	public function getDescription(){
+		return $this->carService->getDescription() . ', and tire rotation';
+	}
+}
+
+
+$service = new TireRotation(BasicInspection);
+
+echo $service->getDescription();
+
+
+?>
