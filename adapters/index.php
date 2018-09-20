9cb1a082a546e30093e7f24d9bbc280a9b94b0f1
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-09-20 16:36:25 +0200

    add adapters design pattern example and autoload, and move decoration into separated directory

---
+++ adapters/index.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+use Acme\Book;
+use Acme\BookInterface;
+use Acme\Kindle;
+use Acme\KindleAdapter;
+
+
+class Person {
+	public function read(BookInterface $book)
+	{
+		$book->open();
+		$book->turnPage();
+	}
+}
+
+(new Person)->read(new KindleAdapter(new Kindle));
